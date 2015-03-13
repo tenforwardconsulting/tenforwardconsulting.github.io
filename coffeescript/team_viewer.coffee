b48f707660d5054665b6a60d052445eca0a2a9e6
Brian Samson <brian@briansamson.com>
2015-03-13 15:13:34 -0500

    add bios to main website

--- coffeescript/team_viewer.coffee
+++ coffeescript/team_viewer.coffee
@@ -2,17 +2,21 @@ window.TenForward ||= {}
 
 class TenForward.TeamViewer
   constructor: ->
-    #@initListeners()
+    @initListeners()
 
   initListeners: ->
     $('.bio-button').on 'click', @showBio
     $('.bio .close').on 'click', @hideBio
+    $('body').on 'click', @hideBio
 
   showBio: (ev) ->
     $('.bio-buttons').hide()
     name = $(ev.target).data().name
     $(".bio[data-name='#{name}']").show()
+    ev.stopPropagation()
 
   hideBio: (ev) ->
-    $('.bio-buttons').show()
-    $(".bio").hide()
+    $target = $(ev.target)
+    if $target.hasClass('close') || $target.closest('.bio').length is 0
+      $('.bio-buttons').show()
+      $(".bio").hide()
--- javascript/team_viewer.js
+++ javascript/team_viewer.js
@@ -3,23 +3,31 @@
   window.TenForward || (window.TenForward = {});
 
   TenForward.TeamViewer = (function() {
-    function TeamViewer() {}
+    function TeamViewer() {
+      this.initListeners();
+    }
 
     TeamViewer.prototype.initListeners = function() {
       $('.bio-button').on('click', this.showBio);
-      return $('.bio .close').on('click', this.hideBio);
+      $('.bio .close').on('click', this.hideBio);
+      return $('body').on('click', this.hideBio);
     };
 
     TeamViewer.prototype.showBio = function(ev) {
       var name;
       $('.bio-buttons').hide();
       name = $(ev.target).data().name;
-      return $(".bio[data-name='" + name + "']").show();
+      $(".bio[data-name='" + name + "']").show();
+      return ev.stopPropagation();
     };
 
     TeamViewer.prototype.hideBio = function(ev) {
-      $('.bio-buttons').show();
-      return $(".bio").hide();
+      var $target;
+      $target = $(ev.target);
+      if ($target.hasClass('close') || $target.closest('.bio').length === 0) {
+        $('.bio-buttons').show();
+        return $(".bio").hide();
+      }
     };
 
     return TeamViewer;
